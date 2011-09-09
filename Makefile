54ec32659afd7a9be3287d4a69181838d4a48282
Dustin Lundquist <dustin@null-ptr.net>
2011-09-09 16:08:15 -0700

    Parse router advertisement packets and options

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CC = gcc
-CFLAGS = -std=c99 -Wall -Wextra -pedantic
+CFLAGS = -std=c99 -Wall -Wextra -pedantic -D_XOPEN_SOURCE
 
 all: routeradv_listend
 
--- routeradv_listend.c
+++ routeradv_listend.c
@@ -8,140 +8,204 @@
 #include <unistd.h> /* getopt() */
 #include <net/if.h> /* if_nametoindex() */
 #include <netinet/icmp6.h> /* ICMP6 structures */
+#include <stdlib.h> /* exit() */
 
 
- 
+
 #define LEN 1000
- 
-void hexdump(const void *, size_t);
+
+void hexdump(const void *, ssize_t);
 void parse(void *, size_t);
 void usage();
- 
+
 int main(int argc, char **argv) {
-	struct icmp6_filter myfilt;
-        struct ipv6_mreq mreq;  /* Multicast address join structure */
-	char buffer[LEN];
-	size_t len;
-	int iface = 0;
-	int ch, fd;
+    struct icmp6_filter myfilt;
+    struct ipv6_mreq mreq;  /* Multicast address join structure */
 
-	while ((ch = getopt(argc, argv, "i:")) != -1) {
-		switch (ch) {
-			case 'i':
-				iface = if_nametoindex(optarg);
-				printf("Interface %s specified, index %d\n", optarg, iface);
-				if (iface == 0) {
-					perror("if_nametoindex()");
-					return(1);
-				}
-				break;
-			default:
-				usage();
-				return(1);
-		}
-	}
+    struct sockaddr_in6 source_addr;
+    socklen_t source_addr_len;
+
+    char buffer[LEN];
+    char address_str[INET6_ADDRSTRLEN];
+    ssize_t len;
+    int iface = 0;
+    int ch, fd;
+
+    while ((ch = getopt(argc, argv, "i:")) != -1) {
+        switch (ch) {
+            case 'i':
+                iface = if_nametoindex(optarg);
+                printf("Interface %s specified, index %d\n", optarg, iface);
+                if (iface == 0) {
+                    perror("if_nametoindex()");
+                    return(1);
+                }
+                break;
+            default:
+                usage();
+                return(1);
+        }
+    }
 
 
- 
-	fd = socket(AF_INET6, SOCK_RAW, IPPROTO_ICMPV6);
-	if (fd < 0) {
-		perror("socket()");
-		return 1;
-	}
- 
- 
-	/* Filter to permit only router advertisements */
-	memset(&myfilt, 0, sizeof(myfilt));
-	ICMP6_FILTER_SETBLOCKALL(&myfilt);
-	ICMP6_FILTER_SETPASS(ND_ROUTER_ADVERT, &myfilt);
-	if (setsockopt(fd, IPPROTO_ICMPV6, ICMP6_FILTER, &myfilt, sizeof(myfilt)) != 0) {
-		perror("setsockopt()");
-		return 1;
-	}
 
-	/* Specify multicast address to listen to */
-	memset(&mreq, 0, sizeof(mreq));
-	mreq.ipv6mr_interface = iface;
-	if (inet_pton(AF_INET6, "ff02::1", &mreq.ipv6mr_multiaddr) != 1) {
-		perror("inet_pton()");
-		return 1;
-	}
-	if (setsockopt(fd, IPPROTO_IPV6, IPV6_JOIN_GROUP, &mreq, sizeof(mreq)) != 0) {
-		perror("setsockopt()");
-		return 1;
-	}
- 
-	for (;;) {
-		len = recv(fd, buffer, LEN, 0);
-		if (len == -1) {
-			perror("recv()");
-			return 1;
-		}
-		parse(buffer, len);
- 
-		hexdump(buffer, len);
-	}
- 
- 
-	return 0;
+    fd = socket(AF_INET6, SOCK_RAW, IPPROTO_ICMPV6);
+    if (fd < 0) {
+        perror("socket()");
+        return 1;
+    }
+
+    /* Filter to permit only router advertisements */
+    memset(&myfilt, 0, sizeof(myfilt));
+    ICMP6_FILTER_SETBLOCKALL(&myfilt);
+    ICMP6_FILTER_SETPASS(ND_ROUTER_ADVERT, &myfilt);
+    if (setsockopt(fd, IPPROTO_ICMPV6, ICMP6_FILTER, &myfilt, sizeof(myfilt)) != 0) {
+        perror("setsockopt()");
+        return 1;
+    }
+
+    /* Specify multicast address to listen to */
+    memset(&mreq, 0, sizeof(mreq));
+    mreq.ipv6mr_interface = iface;
+    if (inet_pton(AF_INET6, "ff02::1", &mreq.ipv6mr_multiaddr) != 1) {
+        perror("inet_pton()");
+        return 1;
+    }
+    if (setsockopt(fd, IPPROTO_IPV6, IPV6_JOIN_GROUP, &mreq, sizeof(mreq)) != 0) {
+        perror("setsockopt()");
+        return 1;
+    }
+
+    /* main loop */ 
+    for (;;) {
+        source_addr_len = sizeof(source_addr);
+        len = recvfrom(fd, buffer, LEN, 0, (struct sockaddr *)&source_addr, &source_addr_len);
+        if (len == -1) {
+            perror("recvfrom()");
+            return 1;
+        }
+        inet_ntop(AF_INET6, &(source_addr.sin6_addr), address_str, INET6_ADDRSTRLEN);
+
+        fprintf(stderr, "Received %zd bytes from %s\n", len, address_str);
+
+        parse(buffer, len);
+    }
+
+
+    return 0;
 }
 
 void
 parse(void *pkt, size_t len) {
-	if (len < sizeof(struct icmp6_hdr)) {
-		fprintf(stderr, "Did not receive complete ICMP packet\n");
-		return;
-	}
-	struct icmp6_hdr *hdr = (struct icmp6_hdr *)pkt;
+    int tries = 10;
+    size_t parsed_len = 0;
+    if (len < sizeof(struct icmp6_hdr)) {
+        fprintf(stderr, "Did not receive complete ICMP packet\n");
+        return;
+    }
+    struct icmp6_hdr *hdr = (struct icmp6_hdr *)pkt;
 
-	switch(hdr->icmp6_type) {
-		case ND_ROUTER_ADVERT:
-			if (len < sizeof(struct nd_router_advert)) {
-				fprintf(stderr, "Did not receive complete ICMP packet\n");
-				return;
-			}
-			struct nd_router_advert *ra = (struct nd_router_advert *)pkt;
+    /* TODO verify checksum */
 
-			fprintf(stderr, "RA\ntype:\t%d\ncode:\t%d\nchsum:\t%d\nhoplimit\t%d\nmanaged\t%d\nother\t%d\nha\t%d\nlifetime\t%d\n",
-				ra->nd_ra_type,
-				ra->nd_ra_code,
-				ra->nd_ra_cksum,
-				ra->nd_ra_curhoplimit,
-				ra->nd_ra_flags_reserved & ND_RA_FLAG_MANAGED, 
-				ra->nd_ra_flags_reserved & ND_RA_FLAG_OTHER,
-				ra->nd_ra_flags_reserved & ND_RA_FLAG_HA,
-				ra->nd_ra_router_lifetime);
+    switch(hdr->icmp6_type) {
+        case ND_ROUTER_ADVERT:
+            if (len < sizeof(struct nd_router_advert)) {
+                fprintf(stderr, "Did not receive complete ICMP packet\n");
+                return;
+            }
+            struct nd_router_advert *ra = (struct nd_router_advert *)pkt;
 
-			
-			break;
-		default:
-			fprintf(stderr, "Unsupported ICMP type\n");
-	}
+            fprintf(stderr, "RA\ntype:\t%d\ncode:\t%d\nchsum:\t%d\nhoplimit\t%d\nmanaged\t%d\nother\t%d\nlifetime\t%d\nreachable\t%d\nretransmit\t%d\n",
+                    ra->nd_ra_type,
+                    ra->nd_ra_code,
+                    ra->nd_ra_cksum,
+                    ra->nd_ra_curhoplimit,
+                    ra->nd_ra_flags_reserved & ND_RA_FLAG_MANAGED, 
+                    ra->nd_ra_flags_reserved & ND_RA_FLAG_OTHER,
+                    ra->nd_ra_router_lifetime,
+                    ra->nd_ra_reachable,
+                    ra->nd_ra_retransmit);
+
+            parsed_len = sizeof(struct nd_router_advert);
+
+            /* Now read the options */
+            while (len - parsed_len >= sizeof(struct nd_opt_hdr) && tries --) {
+                struct nd_opt_hdr *opt = (struct nd_opt_hdr *)((char *)ra + parsed_len);
+
+                switch(opt->nd_opt_type) {
+                    case ND_OPT_SOURCE_LINKADDR:
+                        /* Source Link layer address */
+                        break;
+                    case ND_OPT_PREFIX_INFORMATION:
+                        if (len < sizeof(struct nd_opt_prefix_info)) {
+                            fprintf(stderr, "Did not receive complete ICMP packet option\n");
+                            return;
+                        }
+                        struct nd_opt_prefix_info *pi = (struct nd_opt_prefix_info *)opt;
+                        char prefix_str[INET6_ADDRSTRLEN];
+
+                        inet_ntop(AF_INET6, &(pi->nd_opt_pi_prefix), prefix_str, INET6_ADDRSTRLEN);
+
+                        fprintf(stderr, "Prefix Info\nprefix\t%s/%d\nonlink\t%d\nauto\t%d\nraddr\t%d\nvalid_type\t%d\npreferred_time\t%d\n",
+                                prefix_str,
+                                pi->nd_opt_pi_prefix_len,
+                                pi->nd_opt_pi_flags_reserved & ND_OPT_PI_FLAG_ONLINK,
+                                pi->nd_opt_pi_flags_reserved & ND_OPT_PI_FLAG_AUTO,
+                                pi->nd_opt_pi_flags_reserved & ND_OPT_PI_FLAG_RADDR,
+                                pi->nd_opt_pi_valid_time,
+                                pi->nd_opt_pi_preferred_time);
+
+                        break;
+                    case ND_OPT_MTU:
+                        if (len < sizeof(struct nd_opt_mtu)) {
+                            fprintf(stderr, "Did not receive complete ICMP packet option\n");
+                            return;
+                        }
+                        struct nd_opt_mtu *mtu= (struct nd_opt_mtu *)opt;
+
+                        fprintf(stderr, "MTU %d\n", ntohl(mtu->nd_opt_mtu_mtu));
+                        break;
+                    default:
+                        fprintf(stderr, "Unsupported option %d\n", opt->nd_opt_type);
+                        hexdump(pkt, len);
+                        exit(99);
+                }
+                parsed_len += opt->nd_opt_len * 8;
+            }
+
+            if (parsed_len != len) {
+                fprintf(stderr, "%zd trailing bytes\n", len - parsed_len);
+            }
+
+            break;
+        default:
+            fprintf(stderr, "Unsupported ICMP type\n");
+    }
 }
- 
- 
- 
- 
+
+
+
+
 void
-hexdump(const void *ptr, size_t buflen) {
-	const unsigned char *buf = (const unsigned char*)ptr;
-	int i, j;
-	for (i = 0; i < buflen; i += 16) {
-		printf("%06x: ", i);
-		for (j = 0; j < 16; j ++) 
-			if (i + j < buflen)
-				printf("%02x ", buf[i + j]);
-			else
-				printf("   ");
-		printf(" ");
-		for (j = 0; j < 16; j ++) 
-			if (i + j < buflen)
-				printf("%c", isprint(buf[i + j]) ? buf[i + j] : '.');
-		printf("\n");
-	}
+hexdump(const void *ptr, ssize_t buflen) {
+    const unsigned char *buf = (const unsigned char*)ptr;
+    int i, j;
+    for (i = 0; i < buflen; i += 16) {
+        printf("%06x: ", i);
+        for (j = 0; j < 16; j ++) 
+            if (i + j < buflen)
+                printf("%02x ", buf[i + j]);
+            else
+                printf("   ");
+        printf(" ");
+        for (j = 0; j < 16; j ++) 
+            if (i + j < buflen)
+                printf("%c", isprint(buf[i + j]) ? buf[i + j] : '.');
+        printf("\n");
+    }
 }
 
 void
 usage() {
-	fprintf(stderr, "usage()\n");
+    fprintf(stderr, "usage()\n");
 }
